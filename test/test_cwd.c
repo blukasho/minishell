c526c5b925ecbd58a4ddb918c99631119a565993
blukasho <bodik1w@gmail.com>
2019-05-24 21:32:02 +0300

    test

--- test/test_cwd.c
+++ test/test_cwd.c
@@ -1,25 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test_getcwd.c                                      :+:      :+:    :+:   */
+/*   test_cwd.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: blukasho <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/24 13:35:32 by blukasho          #+#    #+#             */
-/*   Updated: 2019/05/24 14:25:16 by blukasho         ###   ########.fr       */
+/*   Created: 2019/05/24 14:30:25 by blukasho          #+#    #+#             */
+/*   Updated: 2019/05/24 14:33:51 by blukasho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 
 int			main(void)
 {
-	char	*buf;
+	char	buf[4000];
+	ssize_t	i;
 
-	if ((buf = getcwd(NULL, 0)))
-		printf("%s\n", buf);
-	system("leaks -q a.out");
+	i = readlink("/proc/PID/cwd", buf, 4000);
+	printf("%zd %s\n", i, buf);
 	return (0);
 }
